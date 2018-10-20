8754cedbb17109d584c89de0d851b926833163a7
pavelpetrovic <pavel.petrovic@gmail.com>
2018-10-20 05:15:30 +0200

    do log all nxt operations to allow debugging its unreliable behavior

--- modules/live/nxt.c
+++ modules/live/nxt.c
@@ -19,7 +19,8 @@ static volatile int nxt_key_arrived;
 
 void process_response(char *buf)
 {
-  if (strcmp(buf, "Start") == 0) nxt_online = 1;
+  mikes_log_str(ML_DEBUG, "nxt response: ", buf);
+  if (strcmp(buf, "Start") == 0) { nxt_online = 1; mikes_log(ML_INFO, "nxt is online"); }
   else if (strcmp(buf, "Stop") == 0) nxt_online = 0;
   else if (strcmp(buf, "Done") == 0) nxt_command_done = 1;
   else if (strcmp(buf, "Off") == 0) return;
@@ -35,6 +36,7 @@ void *nxt_thread(void *args)
     char *bufp = buf;
     char b;
 
+    mikes_log(ML_INFO, "nxt_thread started");
     while (program_runs)
     {
       if (read(infd[0], &b, 1) == 1)
@@ -57,6 +59,7 @@ void *nxt_thread(void *args)
 
 void init_nxt()
 {
+  mikes_log(ML_INFO, "nxt: init");
   if (opened) return;
   nxt_online = 0;
   nxt_command_done = 0;
@@ -89,22 +92,31 @@ void init_nxt()
   {
       perror("mikes:nxt");
       printf("creating thread for nxt module");
+      mikes_log(ML_ERR, "nxt: creating thread");
   }
   else threads_running_add(1);
   opened = 1;
+  mikes_log(ML_INFO, "nxt: init ok");
 }
 
 void shutdown_nxt()
 {
+  mikes_log(ML_INFO, "nxt: shutdown");
   if (opened) 
   {
     if (write(fd[1], "Quit\n", 5) < 0)
+    {
       perror("nxt write() to pipe");
+      mikes_log(ML_ERR, "nxt: err pipe on shutdown");
+    }
+    printf("nxt: quit sent\n");
     usleep(200000);
     close(fd[1]); 
     close(infd[0]);
     opened = 0;
+    mikes_log(ML_INFO, "nxt: shutdown ok");
   }
+  else mikes_log(ML_WARN, "nxt: shutdown not opened");
 }
 
 int nxt_done()
@@ -142,71 +154,124 @@ void nxt_line(int ln)
   char line[7];
   nxt_command_done = 0;
 
-  if (!opened) return;
-  if (!nxt_online) return;
-  if ((ln > 3) || (ln < 1)) return;
+  mikes_log_val(ML_INFO, "nxt: line", ln);
+
+  if (!opened) 
+  {
+    mikes_log(ML_WARN, "nxt: not opened");
+    return;
+  }
+  if (!nxt_online) 
+  {
+    mikes_log(ML_WARN, "nxt: not online");
+    return;
+  }
+  if ((ln > 3) || (ln < 1)) 
+  {
+    mikes_log(ML_ERR, "nxt: line number error");
+    return;
+  }
   sprintf(line, "Line%d\n", ln);
   if (write(fd[1], line, 6) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
 }
 
 // move the grabber back to home position
 void nxt_dock()
 {
-  if (!opened) return;
-  if (!nxt_online) return;
+  mikes_log(ML_INFO, "nxt: dock");
+  if (!opened) 
+  {
+    mikes_log(ML_WARN, "nxt: not opened");
+    return;
+  }
+  if (!nxt_online) 
+  {
+    mikes_log(ML_WARN, "nxt: not online");
+    return;
+  }
   nxt_command_done = 0;
   if (write(fd[1], "Dock\n", 5) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
 }
 
 // setup fast grabber speed
 void nxt_fast()
 {
+  mikes_log(ML_INFO, "nxt: fast");
   if (!opened) return;
   if (!nxt_online) return;
   if (write(fd[1], "Fast\n", 5) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
 }
 
 // setup slow grabber speed
 void nxt_slow()
 {
+  mikes_log(ML_INFO, "nxt: slow");
   if (!opened) return;
   if (!nxt_online) return;
   if (write(fd[1], "Slow\n", 5) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
 }
 
 // print message on NXT display
 void nxt_message(const char *msg)
 {
+  mikes_log(ML_INFO, "nxt: message");
   if (!opened) return;
   if (!nxt_online) return;
   if (write(fd[1], msg, strlen(msg)) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
   if (write(fd[1], "\n", 1) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write 2 on pipe");
+  }
 }
 
 // clear NXT display
 void nxt_clear_display()
 {
+  mikes_log(ML_INFO, "nxt: clear display");
   if (!opened) return;
   if (!nxt_online) return;
   if (write(fd[1], "Clear\n", 6) < 0)
+  {
     perror("nxt write() on pipe");
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
 }
 
 // read key from NXT (Left, Enter, Right)
 int nxt_read_key()
 {
+  mikes_log(ML_INFO, "nxt: read key");
   if (!opened) return 0;
   if (!nxt_online) return 0;
   nxt_key_arrived = 0;
   if (write(fd[1], "Key\n", 4) < 0)
+  {
     perror("nxt write() on pipe");
-  while (!nxt_key_arrived) usleep(10000);
+    mikes_log(ML_ERR, "nxt: write on pipe");
+  }
+  while (program_runs && !nxt_key_arrived) usleep(10000);
+  mikes_log_val(ML_INFO, "nxt: key:", key);
   return key;
 }
 
