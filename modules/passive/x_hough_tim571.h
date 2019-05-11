67e1f2f07ceda02d5d6af9112c2432d532f5788b
pavelpetrovic <pavel.petrovic@gmail.com>
2019-05-12 00:04:05 +0200

    cleaning x_tim from sick stuff, sick stuff is now in x_tim..._hough_

---
+++ modules/passive/x_hough_tim571.h
@@ -0,0 +1,10 @@
+#ifndef _X_TIM571_H_
+#define _X_TIM571_H_
+
+#define X_TIM571_WIDTH 600
+#define X_TIM571_HEIGHT 600
+
+void init_x_tim571(int max_range_in_mm, int window_update_period_in_ms);
+void shutdown_x_tim571();
+
+#endif
--- modules/passive/x_tim571.c
+++ modules/passive/x_tim571.c
@@ -8,10 +8,6 @@
 #include "../../bites/mikes.h"
 
 #include "../live/tim571.h"
-#include "../live/tim_hough_transform.h"
-#include "../live/line_filter.h"
-#include "../live/tim_segment.h"
-#include "../live/tim_corner.h"
 
 #define ENLARGE_CENTER 50
 
@@ -22,26 +18,12 @@
 #define BASIC_LINE 1
 #define FILTERED_LINE 2
 
-static int show_raw_lines;
-static int show_filtered_lines;
-static int show_segments;
-static int show_corners;
-
 static int win;
 static uint16_t             dist_local_copy[TIM571_DATA_COUNT];
 static uint8_t              rssi_local_copy[TIM571_DATA_COUNT];
 static tim571_status_data   status_local_copy;
-static lines_data           lines_local;
-static lines_data           lines_filtered_local;
-static segments_data        segments_local;
-static corners_data         corners_local;
 static int range;
 static double scale_factor;
-
-static point_2d entry = {
-  .x = 0,
-  .y = 0
-};
 
 void tim571_draw_ray(cairo_t *w, int i, uint16_t d, uint8_t q, int ray_type)
 {
@@ -87,79 +69,6 @@ void tim571_draw_ray(cairo_t *w, int i, uint16_t d, uint8_t q, int ray_type)
    cairo_stroke(w);
 }
 
-void tim571_draw_line(cairo_t *w, line_data *ln, int line_type)
-{
-   uint16_t d = ln->line.distance;
-   double alpha = (90 - ln->line.angle) / 180.0 * M_PI;
-
-   int x = (int)((ENLARGE_CENTER + d) / scale_factor * X_TIM571_WIDTH * 0.45 * sin(alpha) + X_TIM571_WIDTH / 2);
-   int y = (int)((ENLARGE_CENTER + d) / scale_factor * X_TIM571_HEIGHT * 0.45 * cos(alpha) + X_TIM571_HEIGHT / 2);
-
-   if (line_type == BASIC_LINE)
-     cairo_set_source_rgb(w, 0.8, 0.8, 0.3);
-   else
-     cairo_set_source_rgb(w, 0.3, 0.5, 0.5);
-
-   double slen = sqrt((x - X_TIM571_WIDTH / 2) * (x - X_TIM571_WIDTH / 2) + (y - X_TIM571_HEIGHT / 2) * (y - X_TIM571_HEIGHT / 2));
-   if (slen < 0.000001) return;
-
-   double sx = (x - X_TIM571_WIDTH / 2) / slen;
-   double sy = (y - X_TIM571_HEIGHT / 2) / slen;
-
-   int x1 = (int)(x - (-sy) * (X_TIM571_WIDTH + X_TIM571_HEIGHT) + 0.5);
-   int y1 = (int)(y - sx * (X_TIM571_WIDTH + X_TIM571_HEIGHT) + 0.5);
-   int x2 = (int)(x + (-sy) * (X_TIM571_WIDTH + X_TIM571_HEIGHT) + 0.5);
-   int y2 = (int)(y + sx * (X_TIM571_WIDTH + X_TIM571_HEIGHT) + 0.5);
-
-   cairo_move_to(w, x1, X_TIM571_HEIGHT - y1);
-   cairo_line_to(w, x2, X_TIM571_HEIGHT - y2);
-
-   cairo_stroke(w);
-}
-
-void tim571_draw_segment(cairo_t *w, segment_data *segment)
-{
-  vector_2d v1, v2;
-  vector_from_two_points(&entry, &segment->start, &v1);
-  vector_from_two_points(&entry, &segment->end, &v2);
-
-  double d1 = get_vector_length(&v1);
-  double d2 = get_vector_length(&v2);
-
-  double alpha1 = (90 - angle_from_axis_x(&v1)) / 180.0 * M_PI;
-  double alpha2 = (90 - angle_from_axis_x(&v2)) / 180.0 * M_PI;
-
-  int x1 = (int)((ENLARGE_CENTER + d1) / scale_factor * X_TIM571_WIDTH * 0.45 * sin(alpha1) + X_TIM571_WIDTH / 2);
-  int y1 = (int)((ENLARGE_CENTER + d1) / scale_factor * X_TIM571_HEIGHT * 0.45 * cos(alpha1) + X_TIM571_HEIGHT / 2);
-
-  int x2 = (int)((ENLARGE_CENTER + d2) / scale_factor * X_TIM571_WIDTH * 0.45 * sin(alpha2) + X_TIM571_WIDTH / 2);
-  int y2 = (int)((ENLARGE_CENTER + d2) / scale_factor * X_TIM571_HEIGHT * 0.45 * cos(alpha2) + X_TIM571_HEIGHT / 2);
-
-  cairo_set_source_rgb(w, 0.0, 0.0, 0.0);
-
-  cairo_move_to(w, x1, X_TIM571_HEIGHT - y1);
-  cairo_line_to(w, x2, X_TIM571_HEIGHT - y2);
-
-  cairo_stroke(w);
-}
-
-void tim571_draw_corner(cairo_t *w, point_2d *corner)
-{
-  vector_2d v;
-  vector_from_two_points(&entry, corner, &v);
-
-  double d = get_vector_length(&v);
-
-  double alpha = (90 - angle_from_axis_x(&v)) / 180.0 * M_PI;
-
-  int x = (int)((ENLARGE_CENTER + d) / scale_factor * X_TIM571_WIDTH * 0.45 * sin(alpha) + X_TIM571_WIDTH / 2);
-  int y = (int)((ENLARGE_CENTER + d) / scale_factor * X_TIM571_HEIGHT * 0.45 * cos(alpha) + X_TIM571_HEIGHT / 2);
-  cairo_set_source_rgb(w, 0.0, 0.0, 0.0);
-
-  cairo_arc(w, x, X_TIM571_HEIGHT - y, 4, 0, 4 * M_PI);
-  cairo_stroke(w);
-}
-
 void x_tim571_paint(cairo_t *w)
 {
   cairo_push_group(w);
@@ -177,22 +86,6 @@ void x_tim571_paint(cairo_t *w)
     tim571_draw_ray(w, i, d, r, (d == 0) ? RAY_ZERO_TYPE : RAY_USUAL_TYPE);
   }
 
-  if (show_raw_lines)
-    for (int i = 0; i < lines_local.count; i++)
-      tim571_draw_line(w, lines_local.lines + i, BASIC_LINE);
-
-  if (show_filtered_lines)
-    for (int i = 0; i < lines_filtered_local.count; i++)
-      tim571_draw_line(w, lines_filtered_local.lines + i, FILTERED_LINE);
-
-  if (show_segments)
-    for (int i = 0; i < segments_local.count; i++)
-      tim571_draw_segment(w, segments_local.segments + i);
-
-  if (show_corners)
-    for (int i = 0; i < corners_local.count; i++)
-      tim571_draw_corner(w, &corners_local.corners[i].corner);
-
   cairo_pop_group_to_source(w);
   cairo_paint(w);
 }
@@ -203,42 +96,13 @@ void x_tim571_laser_update(uint16_t *dist, uint8_t *rssi, tim571_status_data *st
    memcpy(rssi_local_copy, rssi, sizeof(uint8_t) * TIM571_DATA_COUNT);
 }
 
-void x_tim571_lines_update(tim571_status_data *status_data, uint16_t *distance, uint8_t *rssi, lines_data *lines)
-{
-  lines_local = *lines;
-}
-
-void filtered_lines_update(tim571_status_data *status_data, uint16_t *distance, uint8_t *rssi, lines_data *lines)
-{
-  lines_filtered_local = *lines;
-}
-
-void tim_segments_update(segments_data *segments)
-{
-  segments_local = *segments;
-}
-
-void tim_corners_update(corners_data *corners)
-{
-  corners_local = *corners;
-}
-
 void x_tim571_key_listener(int win, int key)
 {
-    if (key == 'r') show_raw_lines ^= 1;
-    else if (key == 'f') show_filtered_lines ^= 1;
-    else if (key == 's') show_segments ^= 1;
-    else if (key == 'c') show_corners ^= 1;
-    else if (key == GUI_ESC_KEY) program_runs = 0;
+    if (key == GUI_ESC_KEY) program_runs = 0;
 }
 
 void init_x_tim571(int max_range_in_mm, int window_update_period_in_ms)
 {
-   show_raw_lines = 1;
-   show_filtered_lines = 1;
-   show_segments = 1;
-   show_corners = 1;
-
    if (!mikes_config.with_gui) return;
    if (!mikes_config.use_tim571)
    {
@@ -252,10 +116,6 @@ void init_x_tim571(int max_range_in_mm, int window_update_period_in_ms)
    win = gui_open_window(x_tim571_paint, X_TIM571_WIDTH, X_TIM571_HEIGHT, window_update_period_in_ms);
    gui_set_window_title(win, "TIM571");
    register_tim571_callback(x_tim571_laser_update);
-   register_tim_hough_transform_callback(x_tim571_lines_update);
-   register_line_filter_callback(filtered_lines_update);
-   register_tim_segment_callback(tim_segments_update);
-   register_tim_corner_callback(tim_corners_update);
    get_tim571_status_data(&status_local_copy);
    gui_add_key_listener("showing", x_tim571_key_listener);
 }
