4ffa8230054b38c8657f690908ae6addddb6d5e6
PavelPetrovic <pavel.petrovic@gmail.com>
2021-04-23 04:20:17 +0200

    testing and running

--- modules/live/mapping_navig.c
+++ modules/live/mapping_navig.c
@@ -31,9 +31,9 @@ static uint16_t             gauss_dist[TIM571_DATA_COUNT];
 
 static uint16_t				us_data_local_copy[US_COUNT];
 
-static double pose_x;
-static double pose_y;
-static double heading;
+static volatile double pose_x;
+static volatile double pose_y;
+static volatile double heading;
 
 static volatile uint8_t  need_new_data;
 static volatile uint8_t  need_new_pos;
@@ -43,7 +43,7 @@ static int                  fd[2];
 
 
 double target_heading;
-int target_distance = 200;
+int target_distance = 20;
 int traveled_distance;
 double tim_scan_pose_x;
 double tim_scan_pose_y;
@@ -66,35 +66,45 @@ void process_navigation(){
 	{
 		//turn around and go back( or reverse)
 		angle = 180;
+		mikes_log_val(ML_INFO, "NO ARC FOUND", angle);
+
 	}
 	else{
 		int middle_ray = arcs[widest_arc_idx][0]+(arcs[widest_arc_idx][1]-arcs[widest_arc_idx][0])/2;
-		angle = tim571_ray2azimuth(middle_ray);	
+		mikes_log_val(ML_INFO, "middle_ray", middle_ray);
+		angle = (int)(0.5 + tim571_ray2azimuth(middle_ray));	
+		mikes_log_val(ML_INFO, "middle_ray", angle);
+
 		//if (angle<0) angle+=360; 
 	}
-	target_heading = angle / 180 * M_PI - heading;
+	target_heading = angle / 180.0 * M_PI - heading;
 }
 
 void process_movement(){
+		char logstr[100];
+		sprintf(logstr, "process_movement: target %.3lf head %.3lf\n", target_heading / M_PI * 180, heading / M_PI * 180);
+		mikes_log(ML_INFO, logstr);
+
 	if (heading<target_heading+angle_tolerance && heading > target_heading - angle_tolerance)
 	{
 		//set_motor_speeds(0,0);
 		//perform map scan
+
 		double heading_dif = target_heading-heading;
-		int turn_motor_speed = 6+6*((angle_tolerance-fabs(heading_dif))/angle_tolerance);
+		int turn_motor_speed = 3 + 3 * ((angle_tolerance - fabs(heading_dif)) / angle_tolerance);
 		if (heading_dif < 0){
-			set_motor_speeds(turn_motor_speed,12);
+			set_motor_speeds(turn_motor_speed,6);
 		}
 		else{
-			set_motor_speeds(12,turn_motor_speed);
+			set_motor_speeds(6,turn_motor_speed);
 		}
 	}
 	else{
-		if (target_heading<heading){
-			set_motor_speeds(12,-12);
+		if (target_heading < heading){
+			set_motor_speeds(6, -6);
 		}
 		else{
-			set_motor_speeds(-12,12);
+			set_motor_speeds(-6, 6);
 		}
 	}
 	need_new_pos = 1;
@@ -102,13 +112,13 @@ void process_movement(){
 
 uint8_t check_obstacles()
 {
-	double angle = tim_arc_angle/M_PI*180;
-	int ray_start = tim571_azimuth2ray(angle/2);
-	int ray_end = tim571_azimuth2ray(-angle/2);
+	double angle = tim_arc_angle / M_PI * 180;
+	int ray_start = tim571_azimuth2ray(angle / 2);
+	int ray_end = tim571_azimuth2ray(- angle / 2);
 	
-	for (; ray_start<ray_end;ray_start++)
+	for (; ray_start < ray_end; ray_start++)
 	{
-		if (dist_local_copy[ray_start]<target_distance){
+		if (dist_local_copy[ray_start] < target_distance){
 			
 			return 0;
 		}
@@ -123,20 +133,20 @@ void find_arcs(uint16_t *dist, uint16_t arcs[][2], int *arcs_size, int *widest_a
 	uint16_t idx = 0;
 	uint16_t best_arc_size = 0;
 	
-	for(int i=0;i<TIM571_DATA_COUNT;i++)
+	for(int i = 0; i < TIM571_DATA_COUNT; i++)
 	{
 		if (inside_arc)
 		{
-			if (dist[i]<ARC_DIST)
+			if (dist[i] < ARC_DIST)
 			{
-				if (i-arcs[idx][0]>=MIN_ARC_ANGLE*3)
+				if (i - arcs[idx][0] >= MIN_ARC_ANGLE*3)
 				{
 					if(i-arcs[idx][0] > best_arc_size)
 					{
-						best_arc_size = i-arcs[idx][0];
+						best_arc_size = i - arcs[idx][0];
 						*widest_arc_idx = idx;
 					}
-					arcs[idx][1] = i-1;
+					arcs[idx][1] = i - 1;
 					idx++;
 				}
 				inside_arc = 0;
@@ -144,7 +154,7 @@ void find_arcs(uint16_t *dist, uint16_t arcs[][2], int *arcs_size, int *widest_a
 		}
 		else
 		{
-			if (dist[i]>=ARC_DIST)
+			if (dist[i] >= ARC_DIST)
 			{
 				arcs[idx][0] = i;
 				inside_arc = 1;
@@ -152,6 +162,7 @@ void find_arcs(uint16_t *dist, uint16_t arcs[][2], int *arcs_size, int *widest_a
 		}
 	}
 	*arcs_size = idx;
+	mikes_log_val2(ML_INFO, "arcs: ", *arcs_size, best_arc_size);
 }
 
 int choose_best_dir(uint16_t *arcs, int *arcs_size)
@@ -212,7 +223,7 @@ void tim571_newdata_callback(uint16_t *dist, uint8_t *rssi, tim571_status_data *
     memcpy(dist_local_copy, dist, sizeof(uint16_t) * TIM571_DATA_COUNT);
     memcpy(rssi_local_copy, rssi, sizeof(uint8_t) * TIM571_DATA_COUNT);
     alert_new_data(fd);
-    if (traveled_distance>target_distance)
+    if (traveled_distance > target_distance)
     {
 		tim_scan_pose_x = pose_x;
 		tim_scan_pose_y = pose_y;
@@ -228,12 +239,17 @@ void t265_newpos_callback(t265_pose_type *pose, double *new_heading)
 	pose_x = pose->translation.x*100;
 	pose_y = pose->translation.z*100; //?neg z value
 	heading = *new_heading;
+	mikes_log_double(ML_INFO, "t265 x", pose_x);
+	mikes_log_double(ML_INFO, "t265 y", pose_y);
+	mikes_log_double(ML_INFO, "t265 h", *new_heading);
+	
   }
 }
 
 
 void *mapping_navig_thread(void *args)
 {
+  int first_navigation = 1;
   while (program_runs)
   {
     if (wait_for_new_data(fd) < 0) {
@@ -245,8 +261,16 @@ void *mapping_navig_thread(void *args)
 		traveled_distance = sqrt((pose_x-tim_scan_pose_x)*(pose_x-tim_scan_pose_x)+(pose_y-tim_scan_pose_y)*(pose_y-tim_scan_pose_y));
 		need_new_data = 1;
 		need_new_pos = 1;
+		//int chk_obst = check_obstacles();
+		int chk_obst = 1;
+		
+		mikes_log_val(ML_INFO, "mapping_navig obs", chk_obst);
 			
-		if (traveled_distance>target_distance && check_obstacles()){ //make a new scan
+		if (first_navigation || (traveled_distance > target_distance && chk_obst))
+		{ //make a new scan
+			mikes_log(ML_INFO, "mikes:mapping_navig newscan");
+			first_navigation = 0;
+
 			start_scanning();
 			process_navigation();	
 		}
@@ -270,7 +294,7 @@ void init_mapping_navig(){
   need_new_data = 1;
   need_new_pos = 1;
   target_heading = 0;
-  angle_tolerance = 5/180*M_PI;
+  angle_tolerance = 10.0 / 180.0 * M_PI;
   tim_arc_angle = get_arc_angle_in_dist(WHEEL_DIAMETER_IN_MM + 150,target_distance);
   pthread_t t;
   
@@ -289,6 +313,7 @@ void init_mapping_navig(){
     mikes_log(ML_ERR, "creating thread for mapping_navig");
   }
   else threads_running_add(1);
+  mikes_log(ML_INFO, "mikes:mapping_navig initialized");
 }
 
 void shutdown_mapping_navig()
